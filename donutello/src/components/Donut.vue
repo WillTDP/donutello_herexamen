d72c592ae0b1b24c18826d3614b852f7f5f1b3e9
WillTDP <77252197+WillTDP@users.noreply.github.com>
2023-07-05 16:21:20 +0200

    Feat: the topping can be turned off and changed colour

--- donutello/src/components/Donut.vue
+++ donutello/src/components/Donut.vue
@@ -66,6 +66,46 @@ elements.forEach(function(element) {
 });
 
 
+var elements = document.querySelectorAll('.topping');
+elements.forEach(function(element) {
+  element.addEventListener('click', function(e) {
+    e.preventDefault();
+    var colour = this.dataset.colour;
+    console.log(colour);
+
+    // Access the 'glaze' object directly
+    const glazeObject = donut.scene.getObjectByName('glaze');
+
+    // Check if the 'glaze' object is found and is a mesh
+   // Check if the 'glaze' object is found and is an object3D
+  if (glazeObject && glazeObject.isObject3D) {
+    // Traverse the children of the 'glaze' object
+    glazeObject.traverse(function (child) {
+      if (child.name === 'topping' && child.isMesh) {
+        // Modify properties of the 'topping' mesh
+        child.visible = !child.visible;
+        if (child.visible) {
+            //parse the colour from topping-colour
+            var elements = document.querySelectorAll('.topping-colour');
+            elements.forEach(function(element) {
+            element.addEventListener('click', function(e) {
+                e.preventDefault();
+            var colour = this.dataset.colour;
+            console.log(colour);
+             child.material.opacity = 1;
+            child.material.color.setHex(colour);
+            });
+        });
+        } else {
+          child.material.opacity = 0;
+        }
+      }
+    });
+  }
+  });
+});
+
+
 });
 </script>
 
@@ -73,6 +113,11 @@ elements.forEach(function(element) {
   <a href="#" class="recolour" data-colour="0xd52417">Red </a>
   <a href="#" class="recolour" data-colour="0x54A232">Green </a>
   <a href="#" class="recolour" data-colour="0x6fa8dc">Blue </a>
+  
+  <a href="#" class="topping">topping </a>
+  <a href="#" class="topping-colour" data-colour="0xd52417">Red Topping </a>
+  <a href="#" class="topping-colour" data-colour="0x54A232">Green Topping </a>
+  <a href="#" class="topping-colour" data-colour="0x6fa8dc">Blue Topping </a>
 
   <div id="container" style="width: 400px; height: 400px;"></div>
 </template>
