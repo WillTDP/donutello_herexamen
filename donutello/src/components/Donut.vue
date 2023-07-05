f26caebf33928e402284a1f5869ce4a3483499b1
WillTDP <77252197+WillTDP@users.noreply.github.com>
2023-07-05 13:56:49 +0200

    Fix: scene shows up, donut doesnt

--- donutello/src/components/Donut.vue
+++ donutello/src/components/Donut.vue
@@ -1,59 +1,57 @@
 <script setup>
-  import { onMounted } from 'vue';
-  import * as THREE from 'three';
-  import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-  
-  onMounted(() => {
-    let donut = null;
-  
-    const scene = new THREE.Scene();
-      const camera = new THREE.PerspectiveCamera(
-        75,
-        window.innerWidth / window.innerHeight,
-        0.1,
-        1000
-      );
-    
-      const renderer = new THREE.WebGLRenderer();
-      renderer.setSize(window.innerWidth, window.innerHeight);
-      document.body.appendChild(renderer.domElement);
+import { onMounted } from 'vue';
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
-      // add light
-      const light = new THREE.DirectionalLight(0xffffff, 1);
-      light.position.set(0, 0, 1);
-      scene.add(light);
+onMounted(() => {
+  let donut = null;
 
-     // load donut.glb
-     const loader = new THREE.GLTFLoader();
-      loader.load("../assets/donut.glb", function (gltf) {
-        // scale
-        donut = gltf;
-        donut.scene.scale.set(10, 10, 10);
-        scene.add(donut.scene);
-      });
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
 
-      camera.position.z = 5;
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.getElementById('container').appendChild(renderer.domElement);
 
-      function animate() {
-        requestAnimationFrame(animate);
+  // add light
+  const light = new THREE.DirectionalLight(0xffffff, 1);
+  light.position.set(0, 0, 1);
+  scene.add(light);
 
-        renderer.render(scene, camera);
+  // load donut.glb
+  const loader = new GLTFLoader();
+  loader.load('./assets/donut.glb', function (gltf) {
+    // scale
+    donut = gltf;
+    donut.scene.scale.set(10, 10, 10);
+    scene.add(donut.scene);
+  });
+
+  camera.position.z = 5;
+
+  function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+  }
+
+  animate();
+
+  document.querySelector('#recolor').addEventListener('click', function (e) {
+    e.preventDefault();
+    // recolor
+    donut.scene.traverse(function (child) {
+      if (child.isMesh) {
+        child.material.color.setHex(Math.random() * 0xffffff);
       }
-
-      animate();
-  
-     document
-        .querySelector("#recolor")
-        .addEventListener("click", function (e) {
-          e.preventDefault();
-          // recolor
-          donut.scene.traverse(function (child) {
-            if (child.isMesh) {
-              child.material.color.setHex(Math.random() * 0xffffff);
-            }
-          });
-        });
-  </script>
+    });
+  });
+});
+</script>
   
   <template>
     <div id="container" style="width: 400px; height: 400px;"></div>
