a6e58450aa394fde44176a60ccabf428d8bc78dd
WillTDP <77252197+WillTDP@users.noreply.github.com>
2023-08-18 20:00:00 +0200

    Feat: order page gets correct info from API about the specific donut ordered (and displays it)

--- donutello/src/components/Order.vue
+++ donutello/src/components/Order.vue
@@ -1,25 +1,30 @@
 <script setup>
 import { onMounted, ref } from 'vue';
+import { useRoute } from 'vue-router';
+
+//get the route
+const route = useRoute();
 
 //define the variables
 const colour = ref('');
 const topping = ref('no');
-const id = ref(null);
 
 //fetch the data from the API
 onMounted(async () => {
-  try {
-    // delay the fetch request by 2 seconds
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-    const response = await fetch('https://donutello-api-rc87.onrender.com/donut/');
-    const data = await response.json();
-    console.log('API Response:', data);
-    colour.value = data.colour;
-    topping.value = data.topping;
-    id.value = data.id;
-  } catch (error) {
-    console.error('Error fetching data:', error);
-  }
+    const id = route.params.id;
+    console.log('Fetching donut with ID:', id);
+
+    try {
+        // delay the fetch request by 2 seconds
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+        const response = await fetch(`https://donutello-api-rc87.onrender.com/donut/${id}`);
+        const data = await response.json();
+        console.log('API Response:', data);
+        colour.value = data.colour;
+        topping.value = data.topping;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
 });
 
 
@@ -29,7 +34,7 @@ onMounted(async () => {
 
 <template>
     <div>
-        <h1>Order</h1>
+        <h1>Order {{ route.params.id }}</h1>
         <p>Order your donut here!</p>
     </div>
 </template>
